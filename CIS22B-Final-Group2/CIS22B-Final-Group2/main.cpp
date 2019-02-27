18950f20cd07c70cce04a9218ff5019a6a0c70e9
olivec <olivierchanmoy@yahoo.com>
2019-02-26 19:31:39 -0800

    Rename project

---
+++ CIS22B-Final-Group2/CIS22B-Final-Group2/main.cpp
@@ -0,0 +1,19 @@
+/*
+CIS 22B
+Final: Serendipity Booksellers POS System
+Group 2:
+	Olivier Chan Sion Moy
+	Luis Guerrero
+	Natalia Coelho Mendonca
+	Samuel Ruiz Cervantes
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int main()
+{
+	std::cin.get();
+	return 0;
+}
