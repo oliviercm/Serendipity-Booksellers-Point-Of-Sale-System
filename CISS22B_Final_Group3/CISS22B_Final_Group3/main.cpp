49aeb6e19856fad48c628e1ed90f341f06a2970a
olivec <olivierchanmoy@yahoo.com>
2019-02-26 18:45:24 -0800

    Initial VS Run

---
+++ CISS22B_Final_Group3/CISS22B_Final_Group3/main.cpp
@@ -0,0 +1,20 @@
+/*
+CIS 22B
+Final: Serendipity Booksellers POS System
+Group 3:
+	Olivier Chan Sion Moy
+	Luis Guerrero
+	Natalia Coelho Mendonca
+	Samuel Ruiz Cervantes
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+int main()
+{
+	std::cin.get();
+
+	return 0;
+}
