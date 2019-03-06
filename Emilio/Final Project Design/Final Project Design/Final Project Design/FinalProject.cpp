1117f0b790b024b8b4f422a8a68a5fe1620bc4c5
LEmilio14 <47877642+LEmilio14@users.noreply.github.com>
2019-03-05 19:53:32 -0800

    Implemented enum, switch function and new design

---
+++ Emilio/Final Project Design/Final Project Design/Final Project Design/FinalProject.cpp
@@ -0,0 +1,297 @@
+/*
+CIS 22B
+Final: Serendipity Booksellers POS System
+Group 2:
+Olivier Chan Sion Moy
+Luis Guerrero
+Natalia Coelho Mendonca
+Samuel Ruiz Cervantes
+Manasi Gowda
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<string>
+#include<iomanip>
+#include<fstream>
+#include "InventoryDatabase.h"
+#include "Book.h"
+
+using namespace std;	
+
+void displayMainMenu();
+void displayCashierModule();
+void displayInventoryModule();
+void displayReport();
+void displayInventoryList();
+
+
+int main() {
+	system("mode 120, 50");
+
+	const bool debug = true;
+	if (debug)
+	{
+		InventoryDatabase inventoryDatabase("book.txt");
+		inventoryDatabase.buildInventoryArray();
+	}
+	
+	Book book = Book();
+	/*
+	book.quantity = 1;
+
+	std::cout << book << std::endl;
+
+	std::cout << ++book << std::endl;
+
+	std::cin.get();
+	*/
+	
+	int input, userOption, again;
+	ifstream inputFile("Text.txt");
+	string str;
+	string title, s;
+	
+	enum options {option0, cashierModule, inventoryModule, reportModule, exit};
+	do {
+		displayMainMenu();
+		cout << "Choose an option : ";
+		cin >> input;
+		while (input != 1 && input != 2 && input != 3 && input != 4) {
+			cerr << endl;
+			cerr << "EEROR: Not an option, try again" << endl << endl;
+			cerr << "Choose one of the options: ";
+			cin >> input;
+			cerr << endl;
+		}
+		switch (input)
+		{
+		case cashierModule:
+			displayCashierModule();
+			cin >> userOption;
+			break;
+		case inventoryModule:
+			displayInventoryModule();
+			break;
+		case reportModule:
+			displayReport();
+			cout << "Choose an option: ";
+			cin >> userOption;
+			if (userOption == 1) {
+				displayInventoryList();
+			}
+		case exit:
+			cout << "Thank you, bye..." << endl;
+			break;
+		default:
+			break;
+		}
+
+		cout << "Would you like to see the main manu again?" << endl;
+		cout << "\t[1]Yes	[2]No: ";
+		cout << endl;
+		cin >> again;
+	} while ( again == 1);
+
+	system("pause");
+	return 0;
+}
+
+void displayMainMenu() {
+
+	const string bars = "========================================================================================================================";
+	const string title = "Welcome to Serendipity Booksellers.";
+	const string mainMenuText = "[ MAIN MENU ]";
+	const string cashierModeText = "[ 1 ] CASHIER MODE";
+	const string inventoryText = "[ 2 ] INVENTORY MODE ";
+	const string reportText = "[ 3 ] REPORT MODE ";
+	const string exitText = "[ 4 ] EXIT ";
+
+	cout << endl;
+	cout << setw((bars.length() + title.length()) / 2) << title << endl;
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+	cout << setw((bars.length() + mainMenuText.length())/2) << mainMenuText << endl;
+	cout << endl;
+	cout << setw((bars.length() + cashierModeText.length())/2) << cashierModeText << endl;
+	cout << endl;
+	cout << setw(((bars.length() + inventoryText.length())) - ((bars.length() + cashierModeText.length()))) << inventoryText << endl;
+	cout << endl;
+	cout << setw((bars.length() + reportText.length())/2) << reportText << endl;
+	cout << endl;
+	cout << setw((bars.length() + exitText.length()) / 2) << exitText << endl;
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+
+	return;
+}
+
+void displayCashierModule() {
+
+	const string bars = "========================================================================================================================";
+	const string cashierModeText = "[ CASHIER MODE ] ";
+	const string purchaseText = "[ 1 ] PURCHASE A BOOK ";
+	const string rentText = "[ 2 ] RENT A BOOK ";
+	const string exitText = "[ 4 ] EXIT ";
+
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+	cout << setw((bars.length() + cashierModeText.length())/2) << cashierModeText << endl;
+	cout << endl;
+	cout << setw((bars.length() + purchaseText.length()) / 2) << purchaseText << endl;
+	cout << endl;
+	cout << setw((bars.length() + rentText.length()) / 2) << rentText << endl;
+	cout << endl;
+	cout << setw((bars.length() + exitText.length()) / 2) << exitText << endl;
+	cout << endl;
+	cout <<bars << endl;
+	cout << endl;
+
+	return;
+}
+
+void displayInventoryModule() {
+
+	//Book book = Book("1234567890", "Harry Potter", "J.K Rolling", "blabla", "02/03/2019", 3, 35.29, 30.99);
+	const string bars = "========================================================================================================================";
+	const string inventoryModuleText = "[ INVENTORY ] ";
+	const string bookTitle = "TITLE: ";
+	const string bookAuthor = "AUTHOR: ";
+	const string bookPublisher = "PUBLISHER: ";
+	const string bookDate = "DATE: ";
+	const string bookOnHand = "ON-HAND: ";
+	const string bookWholePrice = "WHOLESALE-PRICE: ";
+	const string bookRetailprice = "RETAIL-PRICE: ";
+	const string bookIsbn = "ISBN: ";
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+	cout << right << setw((bars.length() + inventoryModuleText.length()) / 2) << "[ INVENTORY ] " << endl;
+	cout << endl;
+	cout << left << "TITLE:"
+		<< right << setw(17) << "AUTHOR:"
+		<< right << setw(20) << "PUBLISHER:"
+		<< right << setw(15) << "DATE:"
+		<< right << setw(11) << "ON-HAND:"
+		<< right << setw(21) << "WHOLESALE-PRICE:"
+		<< right << setw(15) << "RETAIL PRICE:"
+		<< right << setw(10) << "ISBN:" << endl;
+	cout << endl;
+	cout << bars;
+
+	for (int i = 0; i < 3; i++) {
+		Book book = InventoryDatabase::inventoryArray[i];
+		cout << left << book.title
+			<< right << setw(5) << "#" << right << setw(12)<< book.author
+			<< right << setw(5) << "#" << right << setw(15) << book.publisher
+			<< right << setw(7) << "#" << right << setw(8) << book.addDate
+			<< right << setw(1) << "#" << right << setw(1) << book.quantity
+			<< right << setw(2) << "#" << right << setw(18) << book.wholesale
+			<< right << setw(2) << "#" << right << setw(14) << book.retail
+			<< right << setw(3) << "#" << right << setw(10) << book.isbn << endl;
+	}
+
+	/*
+	cout << setw((bars.length() + cashierModeText.length()) / 2) << "TITLE: " << " Physics for Scientist and Engineers (9th edition)" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "AUTHOR: " << "Jewett Serway" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "PUBLISHER: " <<"Cengage Learning" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "DATE: " << "02/21/2019" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "ON-HAND: " << "5" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "WHOLESALE-PRICE: " << "155.93" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "RETAIL-PRICE: " << "164.99" << endl << endl
+		<< setw((bars.length() + cashierModeText.length()) / 2) << "ISBN: " << "9781305116405"  << endl;
+	cout << endl;
+	*/
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+
+	return;
+}
+
+void displayReport() {
+	const string bars = "========================================================================================================================";
+	const string reportModuleText = "[ REPORT MODE ] ";
+	const string inventoryListText = "[ 1 ] INVENTORY LIST ";
+	const string wholeSaleText = "[ 2 ] WHOLESALE INVENTORY ";
+	const string retailInventoryText = "[ 3 ] RETAIL INVENTORY ";
+	const string listByQuantityText = "[ 4 ] LIST BY QUANTITY ";
+	const string listByCostText = "[ 5 ] LIST BY COST ";
+	const string listByAgedText = "[ 6 ] LIST BY AGE ";
+	
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+	cout << setw((bars.length() + reportModuleText.length())/2) << reportModuleText << endl;
+	cout << endl;
+	cout << setw((bars.length() + inventoryListText.length()) / 2) << inventoryListText << endl;
+	cout << endl;
+	cout << setw((bars.length() + wholeSaleText.length()) / 2) << wholeSaleText << endl;
+	cout << endl;
+	cout << setw((bars.length() + retailInventoryText.length()) / 2) << retailInventoryText << endl;
+	cout << endl;
+	cout << setw((bars.length() + listByQuantityText.length()) / 2) << listByQuantityText << endl;
+	cout << endl;
+	cout << setw((bars.length() + listByCostText.length()) / 2) << listByCostText << endl;
+	cout << endl;
+	cout << setw((bars.length() + listByAgedText.length()) / 2) << listByAgedText << endl;
+	cout << endl;
+	cout << bars << endl;
+	cout << endl;
+
+	return;
+}
+
+void displayInventoryList() {
+
+	cout << "===================================================" << endl;
+	cout << endl;
+	cout << setw(31) << "[ INVENTORY LIST ] " << endl;
+	cout << endl;
+	cout << setw(5) << "TITLE: " << " Physics for Scientist and Engineers (9th edition)" << endl << endl
+		<< setw(5) << "AUTHOR: " << "Jewett Serway" << endl << endl
+		<< setw(5) << "PUBLISHER: " << "Cengage Learning" << endl << endl
+		<< setw(5) << "DATE: " << "02/21/2019" << endl << endl
+		<< setw(5) << "ON-HAND: " << "5" << endl << endl
+		<< setw(5) << "WHOLESALE-PRICE: " << "155.93" << endl << endl
+		<< setw(5) << "RETAIL-PRICE: " << "164.99" << endl << endl
+		<< setw(5) << "ISBN: " << "9781305116405" << endl;
+	cout << endl;
+
+
+	cout << "===================================================" << endl;
+	cout << endl;
+
+	return;
+}
+
+/*
+void displayInventory() {
+	
+	cout << "===================================================" << endl;
+	cout << endl;
+	cout << right << setw(31) << "[ INVENTORY ] " << endl;
+	cout << endl;
+	cout << left << setw(5) << "TITLE " << right << setw(10) << " AUTHOR " 
+		<< right << setw(10) << " PUBLISHER " << right << setw(10) << " DATE "
+		<< right << setw(10) << " ON-HAND "
+		<< right << setw(10) << " WHOLESALE-PRICE "
+		<< right << setw(10) << " RETAIL-PRICE "
+		<< right << setw(10) << " ISBN " << endl;
+
+	cout << left << setw(5) << "Physics for Scientist and Engineers (9th edition) " << right << setw(10) << " Jewett Serway "
+		<< right << setw(10) << " Cengage Learning " << right << setw(10) << " 02/21/2019 "
+		<< right << setw(10) << " 5 "
+		<< right << setw(10) << " 155.93 "
+		<< right << setw(10) << " 164.99 " 
+		<< right << setw(10) << " 9781305116405 " << endl;
+	
+	cout << "===================================================" << endl;
+	cout << endl;
+
+	return;
+}
+*/
---
+++ Emilio/Final Project Design/Final Project Design/Final Project Design/Report.cpp
@@ -0,0 +1,188 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef REPORT_M
+#define REPORT_M
+#include "Report.h"
+#include "InventoryDatabase.h"
+#include "book.h"
+#include<string>
+#include<fstream>
+
+/*												   	 Constructor
+From main, we must pass the number of titles in the inventory somehow. This is the only way to create a Report object.
+*/
+Report::Report(int num_books, Book *data)
+{
+	wholesale_value = 0;
+	retail_value = 0;
+	nu_books = num_books;
+	inventory = data;
+}
+
+/*												Array of Books Creation Functions
+The purpose of these functions are to dynamically create an array of Book objects. With this, the Report class is going to be able to use
+all its member functions on its internal array. "array_books" will create the new array. "setbookList" will fill in the array with
+a for loop by streaming	in the data from the inventory file. We use the overloaded >> operator to fill in each part of a Book object.
+*/
+Book* Report::array_books()
+{
+	Book *ptrBL = new Book[nu_books];
+	return ptrBL;
+}
+void Report::setbookList()
+{
+	books = array_books();
+
+	for (int i = 0; i < nu_books; i++)
+	{
+		(books + i)->isbn = (inventory + i)->isbn;
+		(books + i)->title = (inventory + i)->title;
+		(books + i)->author = (inventory + i)->author;
+		(books + i)->publisher = (inventory + i)->publisher;
+		(books + i)->addDate = (inventory + i)->addDate;
+		(books + i)->quantity = (inventory + i)->quantity;
+		(books + i)->wholesale = (inventory + i)->wholesale;
+		(books + i)->retail = (inventory + i)->retail;
+	}
+}
+
+/*													Sorting Algorithms
+These algorithms will be used by several public functions of this class. They will sort by quantity, wholesale cost, and number of days
+the books have remained in the inventory. All 3 functions will used the Selection Sort algorithm.
+*/
+void Report::selectionSortQty(Book *books)
+{
+	int startScan, minIndex, minValue;
+
+	for (startScan = 0; startScan < (nu_books - 1); startScan++)
+	{
+		minIndex = startScan;
+		minValue = (books + startScan)->quantity;
+		for (int index = startScan + 1; index < nu_books; index++)
+		{
+			if ((books + index)->quantity < minValue)
+			{
+				minValue = (books + index)->quantity;
+				minIndex = index;
+			}
+		}
+		(books + minIndex)->quantity = (books + startScan)->quantity;
+		(books + startScan)->quantity = minValue;
+	}
+}
+void Report::selectionSortCost(Book *books)
+{
+	int startScan, minIndex;
+	double minValue;
+
+	for (startScan = 0; startScan < (nu_books - 1); startScan++)
+	{
+		minIndex = startScan;
+		minValue = (books + startScan)->wholesale;
+		for (int index = startScan + 1; index < nu_books; index++)
+		{
+			if ((books + index)->wholesale < minValue)
+			{
+				minValue = (books + index)->wholesale;
+				minIndex = index;
+			}
+		}
+		(books + minIndex)->wholesale = (books + startScan)->wholesale;
+		(books + startScan)->wholesale = minValue;
+	}
+}
+void Report::selectionSortAge(Book *books)
+{
+	int startScan, minIndex;
+	std::string minValue;
+
+	for (startScan = 0; startScan < (nu_books - 1); startScan++)
+	{
+		minIndex = startScan;
+		minValue = (books + startScan)->addDate;
+		for (int index = startScan + 1; index < nu_books; index++)
+		{
+			if ((books + index)->addDate < minValue)
+			{
+				minValue = (books + index)->addDate;
+				minIndex = index;
+			}
+		}
+		(books + minIndex)->addDate = (books + startScan)->addDate;
+		(books + startScan)->addDate = minValue;
+	}
+}
+
+/*												Inventory List Function
+A list of information on all books.
+*/
+void Report::getInvList()
+{
+	for (int i = 0; i < nu_books; i++)
+	{
+		std::cout << *(books + i);
+	}
+}
+
+/*											Inventory Wholesale Value Function
+A list of the wholesale value of all the books in the inventory and the total wholesale value of the inventory
+*/
+double Report::getInvWholesaleV()
+{
+	for (int i = 0; i < nu_books; i++)
+	{
+		std::cout << *(books + i);
+		wholesale_value += (books + i)->wholesale;
+	}
+	return wholesale_value;
+}
+
+/*											 Inventory Retail Value Function
+A list of the retail value of all the books in the inventory and the total retail value of the inventory
+*/
+double Report::getInvRetailV()
+{
+	for (int i = 0; i < nu_books; i++)
+	{
+		std::cout << *(books + i);
+		retail_value += (books + i)->retail;
+	}
+	return retail_value;
+}
+
+/*													List by Quantity
+A list of all the books in the inventory, sorted by quantity on hand. The books with the greatest quantity on hand will be listed first
+*/
+void Report::getListbyQty()
+{
+	selectionSortQty(books);
+	getInvList();
+}
+
+/*													List by Cost
+A list of all the books in the inventory, sorted by wholesale cost. The books with the greatest wholesale cost will be listed first
+*/
+void Report::getListbyCost()
+{
+	selectionSortCost(books);
+	getInvList();
+}
+
+/*													List by Age
+A list of all the books in the inventory, sorted by purchase date. The books that have been in the inventory the longest will
+be listed first
+*/
+void Report::getListbyAge()
+{
+	selectionSortAge(books);
+	getInvList();
+}
+
+/*													Destructor
+Delete dynamically allocated array of Book objects to prevent memory leak
+*/
+Report::~Report()
+{
+	delete books;
+}
+
+#endif
