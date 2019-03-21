ce67033994302e4fe21cc17e9caf8f8482f55d58
olivec <olivierchanmoy@yahoo.com>
2019-03-21 02:02:58 -0700

    Prepare final submission file

---
+++ Submission Files/Visual Studio/CIS22B-Final-Group2/Cashier.cpp
@@ -0,0 +1,195 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Cashier.h"
+#include <iostream>
+#include <iomanip>
+
+// Constructor 
+Cashier::Cashier(InventoryDatabase* pD)
+{
+	pInventoryDatabase = pD;
+	cart = std::make_unique<InventoryBook[]>(100); // Change this?
+	cartSize = 0;
+	inv = pInventoryDatabase->getInventoryArray();
+}
+
+// Adds book to cart by prompting the user for the ISBN number 
+void Cashier::addBookToCart(std::string isbnNum) {
+	int invCount = pInventoryDatabase->getInventoryArraySize();
+
+	for (int i = 0; i < invCount; i++) {
+		if (inv[i].isbn == isbnNum) {
+			if (getBookCartIndex(isbnNum) != -1) {
+				cart[getBookCartIndex(isbnNum)].quantity++;
+				inv[i].quantity--;
+			}
+			else {
+				cart[cartSize] = inv[i];
+				cart[cartSize].quantity = 0;
+				cart[cartSize].quantity++;
+				inv[i].quantity--;
+				cartSize++;
+			}
+			return;
+		}
+	}
+}
+// Removes book from cart by prompting the user for the ISBN number 
+void Cashier::removeBookFromCart(std::string isbnNum) {
+	for (int i = 0; i < cartSize; i++) {
+		if (cart[i].isbn == isbnNum && cart[i].quantity > 0) { // Will move these checks to main 
+			cart[i].quantity--;
+			inv[i].quantity++;
+			if (cart[i].quantity <= 0)
+			{
+				//Move all following books 1 space back
+				for (int j = i; j < cartSize; j++)
+				{
+					cart[j] = cart[j + 1];
+				}
+				cartSize--;
+			}
+			return;
+		}
+		else if (cart[i].isbn == isbnNum) {
+			std::cout << "There are no more books of this ISBN number availiable in the cart." << std::endl << std::endl;
+			return;
+		}
+	}
+}
+
+// Returns -1 if the book is not in the array (cart), else returns the index the book is at
+int Cashier::getBookCartIndex(std::string isbnNum) {
+	if (cart == nullptr) {
+		return -1;
+	}
+	for (int i = 0; i < cartSize; i++) {
+		if (cart[i].isbn == isbnNum) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+// Returns price of a book including sales tax 
+double Cashier::bookPrice(std::string isbn) {
+	if (pInventoryDatabase->getBookIndexByIsbn(isbn) != -1) {
+		std::cout << "A book of this isbn number is not in the inventory";
+		return -1;
+	}
+	else {
+		return inv[pInventoryDatabase->getBookIndexByIsbn(isbn)].wholesale * SALES_TAX;
+	}
+}
+
+// Returns the subtotal price of the books in the cart 
+double Cashier::subTotal() {
+	double totalPrice = 0;
+
+	for (int i = 0; i < cartSize; i++) {
+
+		totalPrice += (cart[i].quantity * cart[i].retail); // Check whether we use wholesale or retail 
+
+	}
+
+	return totalPrice;
+}
+
+// Returns the total price of the cart.
+double Cashier::totalPriceOfCart() {
+
+	double totalPriceOfCart = 0;
+	double salesTax;
+
+	for (int i = 0; i < cartSize; i++) {
+
+		totalPriceOfCart += (cart[i].quantity * cart[i].retail); // Check whether we use wholesale or retail 
+
+	}
+
+	salesTax = subTotal() * SALES_TAX;
+
+	totalPriceOfCart += salesTax;
+
+	return totalPriceOfCart;
+}
+
+
+// Checks out books --> reduces quantity in inventory by the amount of books in the cart
+void Cashier::checkout() {
+	for (int i = 0; i < cartSize; i++) {
+		inv[pInventoryDatabase->getBookIndexByIsbn(cart[i].isbn)].quantity -= cart[i].quantity;
+		pInventoryDatabase->addToBookQuantityByIsbn(cart[i].isbn, -1);
+	}
+	clearCart();
+}
+
+std::unique_ptr<InventoryBook[]> Cashier::getCart() const
+{
+	std::unique_ptr<InventoryBook[]> copyCartArray = std::make_unique<InventoryBook[]>(cartSize);
+
+	for (int i = 0; i < cartSize; i++)
+	{
+		copyCartArray[i].isbn = cart[i].isbn;
+		copyCartArray[i].title = cart[i].title;
+		copyCartArray[i].author = cart[i].author;
+		copyCartArray[i].publisher = cart[i].publisher;
+		copyCartArray[i].addDate = cart[i].addDate;
+		copyCartArray[i].quantity = cart[i].quantity;
+		copyCartArray[i].wholesale = cart[i].wholesale;
+		copyCartArray[i].retail = cart[i].retail;
+	}
+
+	return copyCartArray;
+}
+
+// For testing purposes 
+namespace UI {
+	const int TERMINAL_WIDTH = 200;
+}
+void Cashier::printCart() {
+	const std::string bookIsbnText = "ISBN:";
+	const std::string bookTitleText = "TITLE:";
+	const std::string bookQuantityText = "QUANTITY:";
+	const std::string bookPriceText = "PRICE:";
+	const std::string totalPriceText = "TOTAL PRICE: $";
+
+	const size_t columnSpacing = 3;
+
+	const size_t isbnColumnLength = 13 + columnSpacing;
+	const size_t quantityColumnLength = bookQuantityText.length() + columnSpacing;
+	const size_t bookPriceColumnLength = bookPriceText.length() + columnSpacing;
+	const size_t totalPriceColumnLength = totalPriceText.length() + columnSpacing;
+
+	const size_t titleColumnLength = UI::TERMINAL_WIDTH - isbnColumnLength - quantityColumnLength - totalPriceColumnLength - bookPriceColumnLength;
+
+	for (int i = 0; i < cartSize; i++) {
+		std::cout << std::left;
+
+		std::cout << std::setw(isbnColumnLength) << cart[i].isbn
+			<< std::setw(titleColumnLength) << cart[i].title
+			<< std::setw(quantityColumnLength) << cart[i].quantity
+			<< std::setw(totalPriceColumnLength) << cart[i].retail
+			<< std::endl << std::endl;
+	}
+}
+
+void Cashier::printCartForReceipt() {
+
+	for (int i = 0; i < cartSize; i++) {
+
+		std::cout << std::left << "\t" << cart[i].isbn << "  " << std::setw(22) << ((cart[i].title.length() <= 20) ? cart[i].title : cart[i].title.substr(0, 17) + "...");
+		std::cout << std::right << std::setw(8) << std::to_string(cart[i].quantity) + "x" + std::to_string(cart[i].retail).substr(0, std::to_string(cart[i].retail).find(".") + 3) << " = " << std::setw(8) << cart[i].quantity * cart[i].retail << std::endl;
+
+	}
+
+	return;
+}
+
+void Cashier::clearCart() {
+
+	cart.reset();
+	cart = std::make_unique<InventoryBook[]>(100);
+	cartSize = 0;
+
+}
